ad74503785bd4bd5c9ca4672f184dd8895173cf0
Yohan Giarelli <yohan@un-zero-un.fr>
2019-05-23 22:48:00 +0200

    Added coverals and travis config

--- test-setup.js
+++ test-setup.js
@@ -1,10 +1,3 @@
-//require('raf/polyfill');
-/**
- * As jsDom do not support mutationobserver and
- * quill requires mutationobserver, thus a shim is needed
- * */
-//require('mutationobserver-shim');
-
 const enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
